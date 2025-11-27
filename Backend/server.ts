b2f66a365388f2ca5d64f9a27b12cac32d859201
chandanjha34 <jha95375@gmail.com>
2025-11-27 19:46:58 +0530

    cors policy updated again again ++

--- Backend/server.ts
+++ Backend/server.ts
@@ -26,9 +26,9 @@ const allowedOrigins = [
   "https://og-de-vahan-9t4wtekco-chandan-jhas-projects.vercel.app",
   "https://polvahan-jhachanda-chandan-jhas-projects.vercel.app",
   "https://polvahan-4cknyaqlv-chandan-jhas-projects.vercel.app",
-  "polvahan-chandan-jhas-projects.vercel.app",
-  "polvahan-jhachanda-chandan-jhas-projects.vercel.app",
-  "polvahan.vercel.app"
+  "https://polvahan-chandan-jhas-projects.vercel.app",
+  "https://polvahan-jhachanda-chandan-jhas-projects.vercel.app",
+  "https://polvahan.vercel.app"
 ];
 
 app.use(
